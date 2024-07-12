9cf51e3c6c1151a27c9e6fc3725619b8ef0e4480
Soumare <lyfidyma@gmail.com>
2024-07-12 19:30:07 +0000

    💄 F01: LoginPage Add with no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [prenom, setPrenom] = useState('')
+  const handleSubmit = () => {
+    alert("Bonjour "+prenom)
+  }
+  const handleChange = (event) => {
+   setPrenom(event.target.value)
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Bienvenue chez nous !</h2>
+        <h4>Connectez-vous</h4>
+         <form action='submit' onSubmit={handleSubmit}>
+          <input 
+            type='text' 
+            value={prenom} 
+            placeholder='Entrer votre prénom ...'
+            onChange={handleChange}
+            required />
+          <button type='submit'>Accédez à votre espace</button>
+         </form>
       </div>
-      <h1>Vite ++++ React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </>
   )
 }
