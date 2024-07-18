d2860f57cf0bf60598b83afac4f21df5a0c65f0c
Soumare <lyfidyma@gmail.com>
2024-07-18 12:25:14 +0000

    ✨ F03: LOGIN >Add background image

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react'
+import { MdKeyboardArrowRight } from 'react-icons/md'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
 
 
 export default function LoginForm() {
@@ -16,7 +18,7 @@ export default function LoginForm() {
      }
   return (
    <div>
-        <form  onSubmit={handleSubmit}>
+        <LoginFormStyled  onSubmit={handleSubmit}>
             <h2>Bienvenue chez nous !</h2>
             <br />
             <h3>Connectez-vous</h3>
@@ -26,8 +28,21 @@ export default function LoginForm() {
                 value={prenom}
                 onChange={handleChange} 
                 required/>
-            <button type='submit'>Accédez à votre compte</button>
-        </form>
+            <button type='submit'>Accédez à votre compte<MdKeyboardArrowRight/></button>
+        </LoginFormStyled>
         </div>
   )
 }
+
+
+const LoginFormStyled = styled.form`
+    font-family: "Amatic SC", sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    button{
+        font-family: "Open Sans", sans-serif;
+        display: inline-flex;
+    }
+    
+`
