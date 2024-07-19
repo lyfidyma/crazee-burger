a10360945641b0764d34ceee26ffebd8356b7163
Soumare <lyfidyma@gmail.com>
2024-07-19 18:43:36 +0000

    ✨ F03: LOGIN  > LoginForm > Add styling button and input

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -3,8 +3,10 @@ import { MdKeyboardArrowRight } from 'react-icons/md'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { theme } from '../../../theme'
-import { IoPersonCircleOutline } from 'react-icons/io5'
+import { IoChevronForward, IoPersonCircleOutline } from 'react-icons/io5'
 import imgBackground from '../../../img/background.jpg'
+import { FaChevronRight } from 'react-icons/fa'
+import { BiSolidChevronRight } from 'react-icons/bi'
 
 export default function LoginForm() {
     const navigate = useNavigate()
@@ -20,24 +22,31 @@ export default function LoginForm() {
      }
   return (
   
-        <LoginFormStyled  onSubmit={handleSubmit}>
+        <LoginFormStyled action='submit' onSubmit={handleSubmit}>
             <div>
                 <h1>Bienvenue chez nous !</h1>
-                <br />
                 <hr/>
                 <h2>Connectez-vous</h2>
             </div>
-            <div>
-            <IoPersonCircleOutline />
+            <div className='input-with-icon'>
+            <IoPersonCircleOutline className='icon'/>
                 <input 
                     type='text' 
                     placeholder='Entrez votre prénom'
                     value={prenom}
                     onChange={handleChange} 
                     required/>
-                    
-                <button type='submit'>Accédez à mon compte<MdKeyboardArrowRight/></button>
             </div>
+                    
+                <button 
+                    className='button-with-icon' 
+                    type='submit'>
+                    <span>Accédez à mon compte</span>
+                    <BiSolidChevronRight className='icon'/>
+                </button>
+               
+            
+        
         </LoginFormStyled>
         
   )
@@ -45,39 +54,103 @@ export default function LoginForm() {
 
 
 const LoginFormStyled = styled.form`
-    font-family: "Amatic SC", sans-serif;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    text-align: center;
+    max-width: 500px;
+    min-width: 400px;
+    margin: 0px auto;
+    padding: 2.5rem 2rem;
+    border-radius: 5px;
+    font-family: "Amatic SC", cursive;
+    
     
     
     h1{
         color: white;
+        font-size: 40px;
     }
     hr{
-        width: 500px;
-        border-color: ${theme.colors.primary};
+        border: 1.5px solid #f56a2c;
+        margin-bottom: 40px;
     }
     h2{
         color: white;
+        margin: 20px 10px 10px;
+        font-size: 36px;
     }
 
-    div{
+    .input-with-icon{
+        background-color: #ffff;
+        border-radius: 5px;
         display: flex;
-        flex-direction: column;
+        align-items: center;
+        padding: 18px 24px;
+        margin: 18px 0;
+
+        .icon{
+            font-size: 15px;
+            margin-right: 8px;
+            color: #93a2b1;
+        }
+
+        input{
+            border: none;
+            font-size: 15px;
+            color: #17161a;
+            width: 100%;
+        }
+
+        &::placeholder{
+            background: white;
+            color: lightgray;
+        }
+    }
+        .button-with-icon{
+            width: 100%;
+            border: 1px solid red;
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            white-space: nowrap;
+            text-decoration: none;
+            line-height: 1;
+
+            padding: 18px 24px;
+            border-radius: 5px;
+            font-size: 15px;
+            font-weight: 700;
+            color: white;
+            background-color: ${theme.colors.primary};
+            border: 1px solid ${theme.colors.primary};
+            
         
-    }
-    input{
-        font-family: "Open Sans", sans-serif;
-        width: 395px;
-        height: 40px;
-        border-radius: 8px;
-    }
-    button{
-        font-family: "Open Sans", sans-serif;
-        width: 400px;
-        color: ${theme.colors.primary};
-        border-color: ${theme.colors.primary};
-    }
+
+        &:hover:not(:disabled){
+            background-color: white;
+            color: ${theme.colors.primary};
+            border: 1px solid ${theme.colors.primary};
+            transition: all 200ms ease-out;
+        }
     
+
+        &:active{
+            color: white;
+            background-color: ${theme.colors.primary};
+            border: 1px solid ${theme.colors.primary};
+        }
+
+        &:disabled{
+            opacity: 0.6;
+            cursor: not-allowed;
+        }
+
+        .icon{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 15px;
+            margin-left: 10px;
+        }
+    }
+  
 `
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -20,20 +20,23 @@ const LoginPageStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: dark;
-  ::before{
+  background: url(${imgBackground}) rgba(0, 0, 0, 0.7);
+  background-size: cover;
+  background-position: center;
+  background-blend-mode: darken;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  /* ::before{
     content: "";
-    background: url(${imgBackground}) rgba(0, 0, 0, 0.7);
-    background-size: cover;
-    background-position: center;
-    background-blend-mode: darken;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+   
+   
+    
+   
     z-index: -1;
-  }
+  } */
 
 `
 
