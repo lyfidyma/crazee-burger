e329ef151fbfd94c0b4b7c9800c87d61c8ac5888
Soumare <lyfidyma@gmail.com>
2024-07-19 09:50:14 +0000

    ✨ F03: LOGIN > Add logo and styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,7 +2,9 @@ import React, { useRef, useState } from 'react'
 import { MdKeyboardArrowRight } from 'react-icons/md'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
-
+import { theme } from '../../../theme'
+import { IoPersonCircleOutline } from 'react-icons/io5'
+import imgBackground from '../../../img/background.jpg'
 
 export default function LoginForm() {
     const navigate = useNavigate()
@@ -17,20 +19,27 @@ export default function LoginForm() {
         setPrenom(event.target.value)
      }
   return (
-   <div>
+  
         <LoginFormStyled  onSubmit={handleSubmit}>
-            <h2>Bienvenue chez nous !</h2>
-            <br />
-            <h3>Connectez-vous</h3>
-            <input 
-                type='text' 
-                placeholder='Entrez votre prénom ...'
-                value={prenom}
-                onChange={handleChange} 
-                required/>
-            <button type='submit'>Accédez à votre compte<MdKeyboardArrowRight/></button>
+            <div>
+                <h1>Bienvenue chez nous !</h1>
+                <br />
+                <hr/>
+                <h2>Connectez-vous</h2>
+            </div>
+            <div>
+            <IoPersonCircleOutline />
+                <input 
+                    type='text' 
+                    placeholder='Entrez votre prénom'
+                    value={prenom}
+                    onChange={handleChange} 
+                    required/>
+                    
+                <button type='submit'>Accédez à mon compte<MdKeyboardArrowRight/></button>
+            </div>
         </LoginFormStyled>
-        </div>
+        
   )
 }
 
@@ -40,9 +49,35 @@ const LoginFormStyled = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
+    
+    
+    h1{
+        color: white;
+    }
+    hr{
+        width: 500px;
+        border-color: ${theme.colors.primary};
+    }
+    h2{
+        color: white;
+    }
+
+    div{
+        display: flex;
+        flex-direction: column;
+        
+    }
+    input{
+        font-family: "Open Sans", sans-serif;
+        width: 395px;
+        height: 40px;
+        border-radius: 8px;
+    }
     button{
         font-family: "Open Sans", sans-serif;
-        display: inline-flex;
+        width: 400px;
+        color: ${theme.colors.primary};
+        border-color: ${theme.colors.primary};
     }
     
 `
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,11 +1,40 @@
 import React, { useState } from 'react'
 import LoginForm from './LoginForm'
+import imgBackground from '../../../img/background.jpg'
+import styled from 'styled-components'
+import Logo from '../reusable-ui/Logo'
 
 export default function LoginPage() {
     
   return (
-    <LoginForm/>
+    <LoginPageStyled>
+      <Logo/>
+      <LoginForm/>
+    </LoginPageStyled>
   )
 }
+
+const LoginPageStyled = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: dark;
+  ::before{
+    content: "";
+    background: url(${imgBackground}) rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+
+`
 
 
