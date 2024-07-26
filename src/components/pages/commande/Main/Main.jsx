0d38025ad91db8686140957bdb7bb376e54df7b4
Soumare <lyfidyma@gmail.com>
2024-07-26 16:51:33 +0000

    💄 F05: ORDER > MAIN > MENU > Add grid layout

--- src/components/pages/commande/Main/Main.jsx
+++ src/components/pages/commande/Main/Main.jsx
@@ -24,6 +24,7 @@ const MainStyled = styled.div`
 
     display: grid;
     grid-template-columns: 1fr;
+    overflow-y: scroll;
     .basket{
       background: pink;
       
--- src/components/pages/commande/Main/Menu.jsx
+++ src/components/pages/commande/Main/Menu.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
+import { fakeMenu1, fakeMenu2 } from '../../../../fakeData/fakeMenu'
 
 export default function Menu() {
+    const [menu, setMenu] = useState(fakeMenu2)
+
   return (
-    <MenuStyled>Menu</MenuStyled>
+    <MenuStyled>
+        {menu.map((product)=>{
+           return <div className='produit'>{product.title}</div>
+        }
+        )}
+    </MenuStyled>
   )
 }
 
 const MenuStyled = styled.div`
       background: purple;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-row-gap: 60px;
+      padding: 50px 50px 150px;
+      justify-items: center;
+
+      .produit{
+        background: red;
+        width: 240px;
+        height: 330px;
+      }
 `;
