dcd17e6565df92e35335f59dae766277b5384573
Soumare <lyfidyma@gmail.com>
2024-07-29 13:06:06 +0000

    ♻️ F05: ORDER > Menu > Create  <Card />

--- src/components/pages/commande/Main/Menu.jsx
+++ src/components/pages/commande/Main/Menu.jsx
@@ -2,15 +2,24 @@ import React, { useState } from 'react'
 import styled from 'styled-components';
 import { fakeMenu1, fakeMenu2 } from '../../../../fakeData/fakeMenu'
 import { theme } from '../../../../theme';
-import Product from './Product';
+import Card from '../../reusable-ui/Card'
+import { formatPrice } from '../../../../utils/maths'
 
 export default function Menu() {
     const [menu, setMenu] = useState(fakeMenu2)
 
   return (
     <MenuStyled>
-        {menu.map((product)=>{
-           return <Product title={product.title} imageSource={product.imageSource} price={product.price}/>
+        {menu.map(({id, title, imageSource, price})=>{
+           return (
+             <Card 
+                key={id}
+                title={title} 
+                imageSource={imageSource} 
+                leftDescription={formatPrice(price)}
+                />
+                // <Card key={product.id} {...product}/>
+        )
         }
         )}
     </MenuStyled>
@@ -25,23 +34,4 @@ const MenuStyled = styled.div`
       padding: 50px 50px 150px;
       justify-items: center;
       box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-      .produit{
-        background: red;
-        width: 240px;
-        height: 330px;
-
-        .image{
-            border: 1px solid fuchsia;
-            width: 100px;
-            height: auto;
-            img{
-                width: 100%;
-                height: 100%;
-            }
-        }
-      }
-      .description{
-        border: 1px solid fuchsia;
-      }
 `;
