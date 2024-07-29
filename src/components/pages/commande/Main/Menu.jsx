092f81e52b3600501c92d124d839c8bc9f5b86dd
Soumare <lyfidyma@gmail.com>
2024-07-29 10:34:24 +0000

    🚧 F05: ORDER > Menu > Add data

--- src/components/pages/commande/Main/Menu.jsx
+++ src/components/pages/commande/Main/Menu.jsx
@@ -9,7 +9,19 @@ export default function Menu() {
   return (
     <MenuStyled>
         {menu.map((product)=>{
-           return <div className='produit'>{product.title}</div>
+           return <div className='produit'>
+            <div className="image">
+                <img src={product.imageSource} alt={product.title} />
+            </div>
+            <div className="info-text">
+                <div className='title'>{product.title}</div>
+                <div className="description">
+                    <div className="price">{product.price}</div>
+                    <button className='add-button'>Ajouter</button>
+                </div>
+            </div>
+            
+            </div>
         }
         )}
     </MenuStyled>
@@ -29,5 +41,15 @@ const MenuStyled = styled.div`
         background: red;
         width: 240px;
         height: 330px;
+
+        .image{
+            border: 1px solid fuchsia;
+            width: 100px;
+            height: auto;
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
       }
 `;
