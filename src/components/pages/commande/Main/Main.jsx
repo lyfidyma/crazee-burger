7bbd76e748c4a92388a65e46a014ddd93cc5f027
Soumare <lyfidyma@gmail.com>
2024-07-31 19:27:29 +0000

    ✨ F07: MAIN > Add layout with basket

--- src/components/pages/commande/Main/Main.jsx
+++ src/components/pages/commande/Main/Main.jsx
@@ -8,26 +8,50 @@ export default function Main() {
   return (
     <MainStyled className='main'>
       {/* <div className='basket'>Basket</div> */}
-        <Menu/>
-
+        <div className="menu-and-admin">
+          <Menu/>
+                <div className="admin">Admin</div>
+        </div>
       
     </MainStyled>
   )
 }
 
 const MainStyled = styled.div`
+border: 3px solid green;
     background-color: ${theme.colors.background_white};
-    flex: 1;
+    /* flex: 1; */
+    height: calc(95vh - 10vh);
+
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
     display: grid;
     grid-template-columns: 1fr;
-    overflow-y: scroll;
-    .basket{
+    //A activer lorsque le basket est actif ou ajouter 25% à la ligne ci-dessus
+    /* grid-template-columns: 25% 1fr; */
+
+    /* overflow-y: scroll; */
+   
+    /* .basket{
       background: pink;
-      
+    } */
+
+    .menu-and-admin{
+      position: relative;
+      overflow-y: hidden;
+      display: grid;
+      border-bottom-left-radius: ${theme.borderRadius.extraRound};
+      border-bottom-right-radius: ${theme.borderRadius.extraRound}; 
+      .admin{
+        background: red;
+        height: 250px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+   }
     }
    
 `
