284a82997bc53f041ccf92a14a1ecb7c9318ba62
Soumare <lyfidyma@gmail.com>
2024-07-31 09:01:01 +0000

    ♻️ F06: NAVBAR > Add <ToastAdmin />

--- src/components/pages/commande/Navbar/NavbarRightSide.jsx
+++ src/components/pages/commande/Navbar/NavbarRightSide.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import Profile from './Pofile'
 import ToggleButton from '../../reusable-ui/ToggleButton'
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { theme } from '../../../../theme'
+import ToastAdmin from './ToastAdmin'
 
 export default function NavbarRightSide({username}) {
 
@@ -34,7 +34,7 @@ export default function NavbarRightSide({username}) {
         onToggle={displayToastNotification}
       />
         <Profile username={username}/>
-        <ToastContainer className="toaster" bodyClassName="body-toast" />
+        <ToastAdmin/>
     </NavbarRightSideStyled>
   )
 }
@@ -43,23 +43,4 @@ const NavbarRightSideStyled = styled.div`
     display: flex;
     align-items: center;
     padding-right: 50px;
-
-    .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
-  }
-
 `
