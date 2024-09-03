6ee0737ba7228c7d78ca014121eddd6cd2974c0a
Soumare <lyfidyma@gmail.com>
2024-09-03 16:38:33 +0000

    ♻️ F08: AddForm > Add inputTextConfig.jsx

--- src/components/pages/commande/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/commande/Main/Admin/AdminPanel/AddForm.jsx
@@ -2,12 +2,10 @@ import React, { useContext, useState } from 'react'
 import styled from 'styled-components';
 import OrderContext from '../../../../../../context/OrderContext.jsx';
 import Input from '../../../../reusable-ui/Input.jsx';
-import { FaHamburger } from 'react-icons/fa';
-import { BsFillCameraFill } from 'react-icons/bs';
-import { MdOutlineEuro } from 'react-icons/md';
 import Button from '../../../../reusable-ui/Button.jsx'
 import ImagePreview from './ImagePreview.jsx';
 import SubmitMessage from './SubmitMessage.jsx';
+import { getInputTextsConfig } from './inputTextConfig.jsx';
 
 export const EMPTY_PRODUCT = {
   id: "",
@@ -20,7 +18,6 @@ export default function Addform() {
 
   //state
   const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext)
-  //const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
   const [isSubmitted, setIsSubmitted] = useState(false)
 
   //comportements
@@ -48,39 +45,26 @@ export default function Addform() {
     }, 2000)
   }
 
+  const inputTexts = getInputTextsConfig(newProduct)
+
   //affichage
   return (
     <AddFormStyled onSubmit={handleSubmit}>
      <ImagePreview imageSource={newProduct.imageSource} title={newProduct.title}/>
       <div className='input-fields'>
-         <Input 
-            name="title"
-            value={newProduct.title}
-            type="text" 
-            placeholder="Nom du produit (ex: Super Burger)"
-            onChange={handleChange}
-            Icon={<FaHamburger />}
-            version="minimalist"
+         {inputTexts.map((input) =>(
+            <Input
+              key = {input.id}
+              // name = {input.name}
+              // value = {input.value}
+              // placeholder = {input.placeholder}
+              // Icon = {input.Icon}
+              //Code ci-dessus remplacer par
+              {...input}
+              onChange = {handleChange}
+              version = "minimalist"
             />
-            
-         <Input
-            name="imageSource"
-            value={newProduct.imageSource} 
-            type="text" 
-            placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
-            onChange={handleChange}
-            Icon={<BsFillCameraFill />}
-            version="minimalist"
-            />
-         <Input 
-          name="price"
-          value={newProduct.price ? newProduct.price : ""}
-          type="text" 
-          placeholder="Prix" 
-          onChange={handleChange}
-          Icon={<MdOutlineEuro />}
-          version="minimalist"
-          />
+         ))}
       </div>
       <div className="submit">
         <Button 
@@ -95,7 +79,6 @@ export default function Addform() {
 }
 
 const AddFormStyled = styled.form`
-  /* border: 2px solid black; */
   display: grid;
   grid-template-columns: 1fr 3fr;
   grid-template-rows: repeat(4, 1fr);
