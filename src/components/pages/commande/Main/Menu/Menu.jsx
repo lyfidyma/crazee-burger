71c16b07897411d66790c00b6f43a521475f0cb2
Soumare <lyfidyma@gmail.com>
2024-10-14 17:05:17 +0000

    🐛 F09: MENU > Disable handleClick if user is not in AdminMode

--- src/components/pages/commande/Main/Menu/Menu.jsx
+++ src/components/pages/commande/Main/Menu/Menu.jsx
@@ -16,6 +16,7 @@ export default function Menu() {
 
     //comportements
     const handleClick = (idProductClicked) => {
+      if(!isModeAdmin) return
       const productClickedOn = menu.find((product) => product.id === idProductClicked)
       setProductSelected(productClickedOn)
   
