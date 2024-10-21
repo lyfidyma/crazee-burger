00453e00516af77493c4b674c7c8d5d6d6168966
Soumare <lyfidyma@gmail.com>
2024-10-21 15:30:41 +0000

    💄 F10: BASKET > Add layout

---
+++ src/components/pages/commande/Main/Basket.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Basket() {
+  return (
+    <BasketStyled>
+        <div className="head">Head</div>
+        <div className="body">Body</div>
+        <div className="footer">Footer</div>
+    </BasketStyled>
+  )
+}
+
+const BasketStyled = styled.div`
+    background: pink;
+    display : flex;
+    flex-direction: column;
+    .head{
+        background: red;
+    }
+  .body{
+        background : blue ;
+  }
+  .footer{
+        background : green
+  }
+`;
---
+++ src/components/pages/commande/Main/MainRightSide.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { theme } from '../../../../theme';
+import styled from 'styled-components';
+import OrderContext from '../../../../context/OrderContext';
+import Admin from './Admin/Admin';
+import Menu from './Menu/Menu';
+
+export default function MainRightSide() {
+    const { isModeAdmin } = useContext(OrderContext)
+  return (
+    <MainRightSideStyled>
+       <Menu />
+       {isModeAdmin && <Admin/> }
+    </MainRightSideStyled>
+  )
+}
+
+const  MainRightSideStyled= styled.div`
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound}; 
+`;
