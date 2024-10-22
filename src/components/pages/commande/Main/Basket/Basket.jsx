c22bd6495f02141d6d25f7b6362ce53b49ca8c64
Soumare <lyfidyma@gmail.com>
2024-10-22 15:38:14 +0000

    💄 F10: BASKET > Add Footer.jsx

--- src/components/pages/commande/Main/Basket/Basket.jsx
+++ src/components/pages/commande/Main/Basket/Basket.jsx
@@ -4,13 +4,15 @@ import { theme } from '../../../../../theme';
 import Header from '../../../reusable-ui/Header';
 import Total from './Total';
 import { formatPrice } from '../../../../../utils/maths';
+import Footer from './Footer';
 
 export default function Basket() {
   return (
     <BasketStyled>
         <Total amountToPay={formatPrice(0)}/>
         <div className="body">Body</div>
-        <Header>Footer</Header>
+        {/* <Header>Footer</Header> */}
+        <Footer />
     </BasketStyled>
   )
 }
