4527eae595553ebd5549d1523d556e715166b4f6
Soumare <lyfidyma@gmail.com>
2024-10-29 18:29:22 +0000

    ♻️ F11 : BASKET > Rename BasketBody to EmptyBasket

--- src/components/pages/commande/Main/Basket/Basket.jsx
+++ src/components/pages/commande/Main/Basket/Basket.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components';
-import { theme } from '../../../../../theme';
 import Total from './Total';
 import { formatPrice } from '../../../../../utils/maths';
 import Footer from './Footer';
-import BasketBody from './BasketBody';
+import EmptyBasket from './EmptyBasket';
+import OrderContext from '../../../../../context/OrderContext';
 
 export default function Basket() {
+  const {basket} = useContext(OrderContext)
   return (
     <BasketStyled>
         <Total amountToPay={formatPrice(0)}/>
-        <BasketBody/>
+        <EmptyBasket basket={basket}/>
         <Footer />
     </BasketStyled>
   )
--- src/components/pages/commande/Main/Basket/EmptyBasket.jsx
+++ src/components/pages/commande/Main/Basket/EmptyBasket.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import styled from 'styled-components';
 import { theme } from '../../../../../theme';
 
-export default function BasketBody() {
+export default function EmptyBasket() {
   return (
-    <BasketBodyStyled>
+    <EmptyBasketStyled>
         <span className="empty-message">Votre commande est vide.</span>
-    </BasketBodyStyled>
+    </EmptyBasketStyled>
   )
 }
 
-const BasketBodyStyled = styled.div`
+const EmptyBasketStyled = styled.div`
     flex: 1;
     background: ${theme.colors.background_white};
     box-shadow: ${theme.shadows.basket};
