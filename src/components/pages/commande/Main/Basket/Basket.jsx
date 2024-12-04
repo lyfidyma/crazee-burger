b4b05c35284cce72a4073e2580c9e1e7c2781dec
Soumare <lyfidyma@gmail.com>
2024-12-04 17:12:02 +0000

    ♻️ F11 : BASKET > Layout > render basketProducts conditionally

--- src/components/pages/commande/Main/Basket/Basket.jsx
+++ src/components/pages/commande/Main/Basket/Basket.jsx
@@ -5,13 +5,17 @@ import { formatPrice } from '../../../../../utils/maths';
 import Footer from './Footer';
 import EmptyBasket from './EmptyBasket';
 import OrderContext from '../../../../../context/OrderContext';
+import BasketProducts from './BasketProducts';
 
 export default function Basket() {
   const {basket} = useContext(OrderContext)
+
+  const isBasketEmpty = basket.length === 0
+
   return (
     <BasketStyled>
         <Total amountToPay={formatPrice(0)}/>
-        <EmptyBasket basket={basket}/>
+        {isBasketEmpty ? <EmptyBasket /> : <BasketProducts/>}       
         <Footer />
     </BasketStyled>
   )
